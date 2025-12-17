8f3d917202639fc7af04c501e505195fa18ade70
Khushi Jain <khushijain.professional@gmail.com>
2025-12-17 22:41:01 +0530

    Describe your change

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import AlertProvider from "./context/AlertProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/crowd-management-system">
       <AlertProvider>
         <App />
       </AlertProvider>
